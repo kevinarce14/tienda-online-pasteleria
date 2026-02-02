4b718e15027efb1111782ab7c309c4ea1772c3ec
Kevin Arce <kevindamian1702@gmail.com>
2026-02-02 16:57:20 -0300

    listo creo

--- backend/src/database/db.py
+++ backend/src/database/db.py
@@ -2,16 +2,31 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# DATABASE_URL = URL.create(
+#     drivername="postgresql",
+#     username="postgres",
+#     password="SqlKevin",
+#     host="localhost",
+#     database="db-tienda-online-pasteleria",
+#     port=5432
+# )
 DATABASE_URL = URL.create(
-    drivername="postgresql",
-    username="postgres",
-    password="SqlKevin",
-    host="localhost",
-    database="db-tienda-online-pasteleria",
-    port=5432
+    drivername="postgresql+psycopg2",
+    username="neondb_owner",
+    password="npg_gRD2wkVuvYH4",
+    host="ep-wispy-breeze-acjxjbvm.sa-east-1.aws.neon.tech",
+    port=5432,
+    database="neondb",
+    query={
+        "sslmode": "require",
+        "options": "-c search_path=pasteleria"
+    }
 )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -21,9 +36,6 @@ SessionLocal = sessionmaker(
 
 Base = declarative_base()
 
-# -------------------------
-# Dependency DB
-# -------------------------
 def get_db():
     db = SessionLocal()
     try:
